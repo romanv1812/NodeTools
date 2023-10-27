42d925942cbad6985b052c6d77f5755475c2bf1f
Roman <83868103+romanv1812@users.noreply.github.com>
2023-10-27 13:19:27 +0300

    Update menu.sh

--- helpfulScripts/menu.sh
+++ helpfulScripts/menu.sh
@@ -9,7 +9,7 @@ while true; do
     read -p "Введите номер опции: " choice
 
     case $choice in
-        1) bash add_user.sh ;;
+        1) . <(wget -qO- https://raw.githubusercontent.com/romanv1812/NodeTools/main/helpfulScripts/add_user.sh) ;;
         2) bash install_go.sh ;;
         3) echo "Выход..."; exit 0 ;;
         *) echo "Неверный выбор!" ;;
