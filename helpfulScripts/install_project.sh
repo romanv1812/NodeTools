e3d56a9701a431fa246cf6d80705557db9769dd8
Roman <83868103+romanv1812@users.noreply.github.com>
2023-10-27 15:18:07 +0300

    Update install_project.sh

--- helpfulScripts/install_project.sh
+++ helpfulScripts/install_project.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# Функция для определения последних 5 версий релизов
+# Функция для определения версий релизов
 function get_latest_releases {
     repo_url="$1"
     
-    # Используем GitHub API для получения информации о релизах
-    releases_json=$(curl -s "https://api.github.com/repos${repo_url}/releases")
+    # Используем предоставленный код для определения версий
+    TAGS=$(wget -qO- "https://api.github.com/repos${repo_url}/releases" | jq '.[] | select(.prerelease==false) | select(.draft==false) | .html_url' | grep -Eo "v[0-9]*\.[0-9]*\.[0-9]*")
     
-    # Извлекаем версии последних 5 релизов
-    latest_releases=$(echo "$releases_json" | jq -r '.[].tag_name' | head -n 5)
-    
-    echo "$latest_releases"
+    echo "$TAGS"
 }
 
 # Получение URL репозитория от пользователя
 read -p "Введите URL репозитория проекта (например, https://github.com/umee-network/umee.git): " repo_url
 
-# Определение последних 5 версий релизов
+# Определение версий релизов
 latest_releases=$(get_latest_releases "$repo_url")
 
 if [ -n "$latest_releases" ]; then
-    echo "Последние 5 версий релизов:"
+    echo "Последние версии релизов:"
     echo "$latest_releases"
 else
-    echo "Не удалось определить версии последних релизов. Проверьте URL репозитория и доступность релизов."
+    echo "Не удалось определить версии релизов. Проверьте URL репозитория и доступность релизов."
 fi
